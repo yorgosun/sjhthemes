e284bdedb1c69766016f2cf3180a828142a00dab
YangJiYun <f111ax@qq.com>
2016-09-27 20:37:11 +0800

    缘理服展

--- index.php
+++ index.php
@@ -28,12 +28,12 @@ $query1 = new WP_Query($postargs);
 $n = 0;
 while ($query1->have_posts()): $query1->the_post();
     ?>
-								<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
-								<?php if ($n == 0) {
+										<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
+										<?php if ($n == 0) {
         echo " class=\"active\"";
     }
     ?>>
-							<?php
+									<?php
     $n++;
 endwhile;
 wp_reset_postdata();
@@ -47,18 +47,18 @@ $n = 0;
 
 while ($query1->have_posts()): $query1->the_post();
     ?>
-						<div class="<?php if ($n == 0) {
+								<div class="<?php if ($n == 0) {
         echo "active ";
     }
     ?>item" data-slide-number="<?php echo $n ?>">
-					<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
-					<div class="carousel-caption">
-					<h1><?php echo get_the_title() ?></h1>
-					<hr>
-					<p><?php echo get_the_excerpt() ?></p>
-					</div>
-					</div>
-					<?php
+							<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
+							<div class="carousel-caption">
+							<h1><?php echo get_the_title() ?></h1>
+							<hr>
+							<p><?php echo get_the_excerpt() ?></p>
+							</div>
+							</div>
+							<?php
     $n++;
 endwhile;
 wp_reset_postdata();
@@ -66,33 +66,6 @@ wp_reset_postdata();
 			</div>
 		</div>
 		<div class="container content-block">
-<<<<<<< Updated upstream
-<a href="/?p=2151">
-			<div class="col-md-3">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu1.jpg" class="img-responsive">
-				<p class="text-center peitu-title">- 缘启 -</p>
-				<p class="peitu-desc">诵经会网站起源于“周日诵经会”项目，该项目在2015年初开始做的时候，是作为龙泉寺信息中心的内部学修</p>
-			</div></a>
-<a href="/?p=2154">
-			<div class="col-md-3">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu2.jpg" class="img-responsive">
-				<p class="text-center peitu-title">- 理念 -</p>
-				<p class="peitu-desc">龙泉寺一直有诵经的传统，师父起初建寺的时候就提倡大家“读诵大乘经典”，法会的时候经常诵经，也是龙泉寺的一个特色</p>
-			</div></a>
-<a href="/?p=2156">
-			<div class="col-md-3">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu3.jpg" class="img-responsive">
-				<p class="text-center peitu-title">- 服务 -</p>
-				<p class="peitu-desc">诵经会网站提供经文中的疑难字、词，电子版经文供大家预习，并提供完整的诵经仪轨、高品质诵经音视频</p>
-			</div></a>
-<a href="/?p=2160">
-			<div class="col-md-3">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu4.jpg" class="img-responsive">
-				<p class="text-center peitu-title">- 展望 -</p>
-				<p class="peitu-desc">2016年我们发愿共同转读《大佛顶首楞严经》，回向一切有情众生，皆有因缘听经闻法</p>
-			</div></a>
-=======
-
 <?php
 $postargs = array(
     'posts_per_page' => 4,
@@ -104,20 +77,20 @@ $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
 
-					<a href="<?php echo get_the_permalink(); ?>">
-						<div class="col-md-3">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu1.jpg" class="img-responsive">
-							<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
-							<p class="peitu-desc"><?php echo get_the_excerpt() ?></p>
-						</div>
-					</a>
+							<a href="<?php echo get_the_permalink(); ?>">
+								<div class="col-md-3">
+									<img src="<?php echo get_template_directory_uri(); ?>/img/index_peitu1.jpg" class="img-responsive">
+									<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
+									<p class="peitu-desc"><?php echo get_the_excerpt() ?></p>
+								</div>
+							</a>
 
-			<?php
+					<?php
 endwhile;
 wp_reset_postdata();
 ?>
 
->>>>>>> Stashed changes
+
 		</div>
 		<div class="container-fluid content-block content-gray">
 		<div class="container">
@@ -194,12 +167,12 @@ $n = 0;
 while ($query2->have_posts()): $query2->the_post();
 
     ?>
-																								<div class="col-md-6">
-																									<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>" width="300" height="183"></a>
-																									<h3><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-																									<p><?php echo get_post_meta(get_the_id(), 'author', true) ?> <?php the_time('Y-m-d')?></p>
-																								</div>
-																				<?php
+																										<div class="col-md-6">
+																											<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>" width="300" height="183"></a>
+																											<h3><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+																											<p><?php echo get_post_meta(get_the_id(), 'author', true) ?> <?php the_time('Y-m-d')?></p>
+																										</div>
+																						<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -229,10 +202,10 @@ while ($query3->have_posts()): $query3->the_post();
 
     ?>
 
-																			    <a href="<?php echo get_the_permalink(); ?>">
-																			    	<div class="booktitle"><?php echo get_the_title(); ?></div>
-																			    <img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
-																				<?php
+																					    <a href="<?php echo get_the_permalink(); ?>">
+																					    	<div class="booktitle"><?php echo get_the_title(); ?></div>
+																					    <img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
+																						<?php
 endwhile;
 wp_reset_postdata();
 ?>
