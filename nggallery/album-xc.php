3b49bdb6cbb46c7c2a9810e0712b0edd06aa2193
YangJiYun <f111ax@qq.com>
2016-09-29 22:07:57 +0800

    模版测试

--- nggallery/album-xc.php
+++ nggallery/album-xc.php
@@ -1,20 +1,22 @@
-<?php $this->start_element('nextgen_gallery.gallery_container', 'container', $displayed_gallery); ?>
+<?php $this->start_element('nextgen_gallery.gallery_container', 'container', $displayed_gallery);?>
+这是相册模版
 <div class="ngg-albumoverview">
-    <?php foreach ($galleries as $gallery) { ?>
+    <?php foreach ($galleries as $gallery) {
+    ?>
         <div class="ngg-album-compact">
             <div class="ngg-album-compactbox">
                 <div class="ngg-album-link">
-                    <?php $this->start_element('nextgen_gallery.album_gallery', 'item', $gallery); ?>
-                    <?php if ($open_gallery_in_lightbox AND $gallery->entity_type == 'gallery'): ?>
+                    <?php $this->start_element('nextgen_gallery.album_gallery', 'item', $gallery);?>
+                    <?php if ($open_gallery_in_lightbox and $gallery->entity_type == 'gallery'): ?>
                         <a
                             <?php echo $gallery->displayed_gallery->effect_code ?>
-                            href="<?php echo esc_attr($gallery->previewpic_fullsized_url)?>"
+                            href="<?php echo esc_attr($gallery->previewpic_fullsized_url) ?>"
                             data-fullsize="<?php echo esc_attr($gallery->previewpic_fullsized_url) ?>"
                             data-src="<?php echo esc_attr($gallery->previewpic_fullsized_url) ?>"
-                            data-thumbnail="<?php echo esc_attr($gallery->previewurl)?>"
-                            data-title="<?php echo esc_attr($gallery->previewpic_image->alttext)?>"
-                            data-description="<?php echo esc_attr(stripslashes($gallery->previewpic_image->description))?>"
-                            data-image-id="<?php echo esc_attr($gallery->previewpic)?>"
+                            data-thumbnail="<?php echo esc_attr($gallery->previewurl) ?>"
+                            data-title="<?php echo esc_attr($gallery->previewpic_image->alttext) ?>"
+                            data-description="<?php echo esc_attr(stripslashes($gallery->previewpic_image->description)) ?>"
+                            data-image-id="<?php echo esc_attr($gallery->previewpic) ?>"
                         >
                             <img class="Thumb"
                                  alt="<?php echo esc_attr($gallery->title); ?>"
@@ -26,33 +28,33 @@
                                  alt="<?php echo esc_attr($gallery->title); ?>"
                                  src="<?php echo nextgen_esc_url($gallery->previewurl); ?>"/>
                         </a>
-                    <?php endif ?>
-                    <?php $this->end_element(); ?>
+                    <?php endif?>
+                    <?php $this->end_element();?>
                 </div>
             </div>
             <?php if (!empty($image_gen_params)) {
-                $max_width = 'style="max-width: ' . ($image_gen_params['width'] + 20) . 'px"';
-            } else {
-                $max_width = '';
-            } ?>
+        $max_width = 'style="max-width: ' . ($image_gen_params['width'] + 20) . 'px"';
+    } else {
+        $max_width = '';
+    }?>
             <h4>
                 <a class="ngg-album-desc"
                    title="<?php echo esc_attr($gallery->title); ?>"
                    href="<?php echo nextgen_esc_url($gallery->pagelink); ?>"
                     <?php echo $max_width; ?>>
-                    <?php echo_safe_html($gallery->title); ?>
+                    <?php echo_safe_html($gallery->title);?>
                 </a>
             </h4>
             <p class="ngg-album-gallery-image-counter">
-                <?php if (isset($gallery->counter) && $gallery->counter > 0) { ?>
-                    <strong><?php echo $gallery->counter; ?></strong>&nbsp;<?php _e('Photos', 'nggallery'); ?>
-                <?php } else { ?>
+                <?php if (isset($gallery->counter) && $gallery->counter > 0) {?>
+                    <strong><?php echo $gallery->counter; ?></strong>&nbsp;<?php _e('Photos', 'nggallery');?>
+                <?php } else {?>
                     &nbsp;
-                <?php } ?>
+                <?php }?>
             </p>
         </div>
-    <?php } ?>
+    <?php }?>
     <br class="ngg-clear"/>
     <?php echo $pagination ?>
 </div>
-<?php $this->end_element(); ?>
+<?php $this->end_element();?>
