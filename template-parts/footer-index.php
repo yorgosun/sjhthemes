07319eae75bdfc8b90802def96af5b6f2f058b86
YangJiYun <f111ax@qq.com>
2016-10-25 21:02:10 +0800

    页脚修改

--- template-parts/footer-index.php
+++ template-parts/footer-index.php
@@ -2,10 +2,10 @@
 
 <div class="col-md-2">
 	<p>关于诵经会</p>
-	<li><a href="#">缘启</a></li>
-	<li><a href="#">诵经会义工召集</a></li>
-	<li><a href="#">联系我们</a></li>
-	<li><a href="#">龙泉之声</a></li>
+	<li><a href="/?p=2151">缘起</a></li>
+	<li><a href="/?p=2677">诵经会义工召集</a></li>
+	<li><a href="/?p=2680">联系我们</a></li>
+	<li><a href="http://www.longquanzs.org/">龙泉之声</a></li>
 	<li><a href="/wp-admin/">后台维护</a></li>
 </div>
 <div class="col-md-2">
