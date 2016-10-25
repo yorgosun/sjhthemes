071f682dd4fbe2ddb13195467d770fe67797a5e8
YangJiYun <f111ax@qq.com>
2016-10-25 21:24:00 +0800

    修改tag

--- tag.php
+++ tag.php
@@ -1,19 +1,19 @@
 <?php
-/*
-Template Name: Jw
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
-?>
-<?php get_header();?>
-<div class="narrowcolumn">
-<div class="post" id="post-<?php the_ID();?>">
-<h2>Tags</h2>
-<div class="entry">
-<?php wp_tag_cloud('smallest=12&largest=12&unit=px&number=5000');?>//smallest是最小字号,largest是最大字号,unit是单位,number是显示关键词个数,默认是45个
-</div>
-</div>
-</div><!-- end narrwocolumn -->
+
+get_header();?>
+
+标签分类
+
+<?php get_sidebar();?>
 <?php get_footer();?>
-<?php
-/*
-Template Name: Jw
- */
