42a5cce2450e8c60d7b10ac8cfe6edbc12c887d4
YangJiYun <f111ax@qq.com>
2017-03-04 15:42:34 +0800

    1

--- category-zbxdhz.php
+++ category-zbxdhz.php
@@ -9,26 +9,26 @@
 
 get_header();?>
 <style>
-    .songjing .nav h1.title{
-        font-size: 55px;
+.songjing .nav h1.title{
+font-size: 55px;
 }
 </style>
 <div class="songjing songjingxinde">
-    <div class="nav">
-        <div class="container">
-            <h1 class="title">
-                诵经心得分享,感悟佛法
-            </h1>
-            <p class="intro" style="text-align: center;">
-                炉香苒苒，梵音琅琅，罄声阵阵，大义洋洋。诵经，不只在深山古刹，也可在红尘凡间
-            </p>
-        </div>
-    </div>
-    <section>
-        <div class="container">
-            <div class="tab1 tab-item">
-                <div class="currentActivity">
-                    <ul class="activities" style="margin-top: 20px;">
+<div class="nav">
+<div class="container">
+<h1 class="title">
+诵经心得分享,感悟佛法
+</h1>
+<p class="intro" style="text-align: center;">
+炉香苒苒，梵音琅琅，罄声阵阵，大义洋洋。诵经，不只在深山古刹，也可在红尘凡间
+</p>
+</div>
+</div>
+<section>
+<div class="container">
+<div class="tab1 tab-item">
+<div class="currentActivity">
+<ul class="activities" style="margin-top: 20px;">
 
 <?php
 wp_reset_postdata();
@@ -46,33 +46,29 @@ while ($query->have_posts()): $query->the_post();
 //图片的尺寸要在common.css里调整，下面img暂时先写样式
 
     ?>
-									<li class="item">
-									<a href="<?php echo get_the_permalink(); ?>">
-									<img  style="height:200px" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
-									<h5><?php echo get_the_title(); ?></h5></a>
-									<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
-							        <p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
-							         <p>
-							         <a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton"  type="button">周报</button></a>
-							        <a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>"><button  class="newbutton" type="button">心得</button></a>
-							        <a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '照片', true) ?>"><button  class="newbutton" type="button">照片</button></a>
-							        </p>
-									</li>
-							<?php
+	<li class="item">
+	<a href="<?php echo get_the_permalink(); ?>">
+	<img  style="height:200px" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
+	<h5><?php echo get_the_title(); ?></h5></a>
+	<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
+	<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
+	<p>
+	<a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton"  type="button">周报</button></a>
+	<a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>"><button  class="newbutton" type="button">心得</button></a>
+	<a style="width:32%" href="<?php echo get_post_meta(get_the_id(), '照片', true) ?>"><button  class="newbutton" type="button">照片</button></a>
+	</p>
+	</li>
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
- </ul>
- </div>
+</ul>
+</div>
 <?php
 wp_pagenavi();
 ?>
- </div>
 </div>
-
-
-
-    </section>
 </div>
-
+</section>
+</div>
 <?php get_footer();?>
--- index.php
+++ index.php
@@ -21,13 +21,13 @@ get_header();
 file_get_contents("http://play.jcloudlv.com/live/61bf0701b8d77529e0.m3u8");
 ?>
 <div class="live" style="display:<?php echo (strpos($http_response_header[0], '404 Not Found') === false) ? 'block' : 'none'; ?>">
-    <a href="http://sutrachanting.longquanzs.org/chatroom/live">
-        <img src="/wp-content/themes/twentysixteen/img/live.gif" />
-    </a>
+<a href="http://sutrachanting.longquanzs.org/chatroom/live">
+<img src="/wp-content/themes/twentysixteen/img/live.gif" />
+</a>
 </div>
 
-	<ol class="carousel-indicators">
-	<?php
+<ol class="carousel-indicators">
+<?php
 $postargs = array(
     'posts_per_page' => 5,
     'category_name' => '首页轮播图',
@@ -38,43 +38,43 @@ $query1 = new WP_Query($postargs);
 $n = 0;
 while ($query1->have_posts()): $query1->the_post();
     ?>
-									<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
-									<?php if ($n == 0) {
+	<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
+	<?php if ($n == 0) {
         echo " class=\"active\"";
     }
     ?>>
-									<?php
+	<?php
     $n++;
 endwhile;
 wp_reset_postdata();
 ?>
-	</ol>
-	<!-- main slider carousel items -->
-	<div class="carousel-inner">
-		<?php
+</ol>
+<!-- main slider carousel items -->
+<div class="carousel-inner">
+<?php
 $query1 = new WP_Query($postargs);
 $n = 0;
 
 while ($query1->have_posts()): $query1->the_post();
     ?>
-									<div class="<?php if ($n == 0) {
+	<div class="<?php if ($n == 0) {
         echo "active ";
     }
     ?>item" data-slide-number="<?php echo $n ?>">
-									<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
-									<div class="carousel-caption">
+	<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
+	<div class="carousel-caption">
 
-									<h1 class="banner-title"><?php echo get_the_title() ?></h1>
-									<hr style="width: 60%">
-									<p class="banner-text"><?php echo get_the_excerpt() ?></p>
-									</div>
-									</div>
-									<?php
+	<h1 class="banner-title"><?php echo get_the_title() ?></h1>
+	<hr style="width: 60%">
+	<p class="banner-text"><?php echo get_the_excerpt() ?></p>
+	</div>
+	</div>
+	<?php
     $n++;
 endwhile;
 wp_reset_postdata();
 ?>
-	</div>
+</div>
 </div>
 <div class="container content-block">
 <img src='/wp-content/themes/twentysixteen/img/introduction.gif'>
@@ -88,41 +88,39 @@ $postargs = array(
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-											<a href="<?php echo get_the_permalink(); ?>">
-												<div class="col-md-3">
-													<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
-													<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
-												</div>
-											</a>
+	<a href="<?php echo get_the_permalink(); ?>">
+	<div class="col-md-3">
+	<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
+	<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
+	</div>
+	</a>
 
-											<?php
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
 </div>
 <div class="container-fluid content-block content-gray">
-	<div class="container">
-		<?php
+<div class="container">
+<?php
 $postargs = array(
     'name' => 'index_lection',
 );
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-											<div class="col-md-5">
-												<h2><?php echo get_the_title() ?></h2>
-												<p class="desctext">
-													<?php echo the_content() ?>
-												</p>
-												<a href="/?cat=208" ><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
-											</div>
-									<div class="col-md-7">
-										<?php
+	<div class="col-md-5">
+	<h2><?php echo get_the_title() ?></h2>
+	<p class="desctext">
+	<?php echo the_content() ?>
+	</p>
+	<a href="/?cat=208" ><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
+	</div>
+	<div class="col-md-7">
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
-
-
 <?php
 $postargs = array(
     'posts_per_page' => 1,
@@ -135,20 +133,18 @@ while ($query1->have_posts()): $query1->the_post();
 ///head c"<?php echo get_the_permalink();
 
     ?>
-																		<div class="col-md-6">
-																			<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
-																			<h3><?php echo get_the_title() ?></h3>
-																			<p>时间：<?php echo get_post_meta(get_the_id(), 'activitytime', true) ?></p>
-																			<p>地点：<?php echo get_post_meta(get_the_id(), 'activitylocation', true) ?></p>
-																			<p>YY直播：<?php echo get_post_meta(get_the_id(), 'yychannel', true) ?></p>
-																			<p><a href="http://forum.longquanzs.org/forum.php?mod=viewthread&tid=80449&fromuid=211674"><button class="newbutton" style="width: 100%;" type="button">报名</button></a></p>
-																		</div>
-																			<?php
+	<div class="col-md-6">
+	<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
+	<h3><?php echo get_the_title() ?></h3>
+	<p>时间：<?php echo get_post_meta(get_the_id(), 'activitytime', true) ?></p>
+	<p>地点：<?php echo get_post_meta(get_the_id(), 'activitylocation', true) ?></p>
+	<p>YY直播：<?php echo get_post_meta(get_the_id(), 'yychannel', true) ?></p>
+	<p><a href="http://forum.longquanzs.org/forum.php?mod=viewthread&tid=80449&fromuid=211674"><button class="newbutton" style="width: 100%;" type="button">报名</button></a></p>
+	</div>
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
-
-
 <?php
 $postargs = array(
     'posts_per_page' => 1,
@@ -160,56 +156,49 @@ $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
 ///head c"<?php echo get_the_permalink();
     ?>
-											<div class="col-md-6">
-												<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>">
-												<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
-												</a>
-												<h3><?php echo get_the_title() ?></h3>
-												<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
-												<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
-												<p>&nbsp;</p>
-												<p>
-												<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton" style="width: 32%;" type="button">周报</button></a>
-												<a href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">心得</button></a>
-												<a href="<?php echo get_post_meta(get_the_id(), '照片', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">照片</button></a>
-												</p>
-											</div>
-												<?php
+	<div class="col-md-6">
+	<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>">
+	<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
+	</a>
+	<h3><?php echo get_the_title() ?></h3>
+	<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
+	<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
+	<p>&nbsp;</p>
+	<p>
+	<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton" style="width: 32%;" type="button">周报</button></a>
+	<a href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">心得</button></a>
+	<a href="<?php echo get_post_meta(get_the_id(), '照片', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">照片</button></a>
+	</p>
+	</div>
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
-
-
-
-
 </div>
 </div>
 </div>
-
-
-
 <div class="container-fluid content-block content-gray-del">
-	<div class="container content-block">
-			<div class="col-md-5">
-				<?php
+<div class="container content-block">
+<div class="col-md-5">
+<?php
 $postargs = array(
     'name' => 'index_tape',
 );
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																																											<h2><?php echo get_the_title() ?></h2>
-																																											<p class="desctext">
-																																												<?php echo the_content() ?>
-																																											</p>
-																																									<?php
+	<h2><?php echo get_the_title() ?></h2>
+	<p class="desctext">
+	<?php echo the_content() ?>
+	</p>
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
-					<a href="/?cat=53" class="button"><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
-			</div>
+<a href="/?cat=53"><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
+</div>
 <div class="col-md-7">
-		<div class="booklist">
+<div class="booklist">
 
 <?php
 $postargs = array(
@@ -223,16 +212,16 @@ $n = 0;
 while ($query3->have_posts()): $query3->the_post();
 
     ?>
-																																						<a href="<?php echo get_the_permalink(); ?>">
-																																							<div class="booktitle"><?php echo get_the_title(); ?></div>
-																																							<img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
-																																					<?php
+	<a href="<?php echo get_the_permalink(); ?>">
+	<div class="booktitle"><?php echo get_the_title(); ?></div>
+	<img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
+	<?php
 endwhile;
 wp_reset_postdata();
 ?>
 
-								</div>
-						</div>
-				</div>
-		</div>
+</div>
+</div>
+</div>
+</div>
 <?php get_footer();?>
