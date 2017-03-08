25038e8b7ba312ec3d0b374cdda429da2452394c
YangJiYun <f111ax@qq.com>
2017-03-08 19:48:21 +0800

    1

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,7 +9,7 @@
 
 ?>
 <h1><?php the_title();?></h1>
-<p>作者：<?php echo get_post_meta(get_the_id(), 'author', true) ?> / 时间：<?php the_time('Y-m-d')?></p>
+<p><?php echo get_post_meta(get_the_id(), 'author', true) ?>  时间：<?php the_time('Y-m-d')?></p>
 <div class="content-text">
 	<?php the_content();?>
 </div>
