8e8f172b2bb0fc2a2c453526ade118ecd5f74106
YangJiYun <f111ax@qq.com>
2017-08-15 22:55:32 +0800

    1

--- index.php
+++ index.php
@@ -38,12 +38,12 @@ $query1 = new WP_Query($postargs);
 $n = 0;
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																	<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
-																	<?php if ($n == 0) {
+																		<img src="<?php echo get_the_post_thumbnail_url(null, array(160, 100)) ?>" data-target="#myCarousel" data-slide-to="<?php echo $n ?>"
+																		<?php if ($n == 0) {
         echo " class=\"active\"";
     }
     ?>>
-																	<?php
+																		<?php
     $n++;
 endwhile;
 wp_reset_postdata();
@@ -57,19 +57,19 @@ $n = 0;
 
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																	<div class="<?php if ($n == 0) {
+																		<div class="<?php if ($n == 0) {
         echo "active ";
     }
     ?>item" data-slide-number="<?php echo $n ?>">
-																	<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
-																	<div class="carousel-caption">
+																		<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
+																		<div class="carousel-caption">
 
-																	<h1 class="banner-title"><?php echo get_the_title() ?></h1>
-																	<hr style="width: 60%">
-																	<p class="banner-text"><?php echo get_the_excerpt() ?></p>
-																	</div>
-																	</div>
-																	<?php
+																		<h1 class="banner-title"><?php echo get_the_title() ?></h1>
+																		<hr style="width: 60%">
+																		<p class="banner-text"><?php echo get_the_excerpt() ?></p>
+																		</div>
+																		</div>
+																		<?php
     $n++;
 endwhile;
 wp_reset_postdata();
@@ -78,9 +78,9 @@ wp_reset_postdata();
 </div>
 <div class="container content-block" style="display:flex;">
 
-<a href="http://sutrachanting.longquanzs.org/?p=9112"><img width="100%" src="/wp-content/themes/twentysixteen/img/t1.png"></a>
+<a href="http://sutrachanting.longquanzs.org/?p=9107"><img width="100%" src="/wp-content/themes/twentysixteen/img/t1.png"></a>
 <a href="http://sutrachanting.longquanzs.org/?p=9109"><img width="100%" src="/wp-content/themes/twentysixteen/img/t2.png"></a>
-<a href="http://sutrachanting.longquanzs.org/?p=9107"><img width="100%" src="/wp-content/themes/twentysixteen/img/t3.png"></a>
+<a href="http://sutrachanting.longquanzs.org/?p=9112"><img width="100%" src="/wp-content/themes/twentysixteen/img/t3.png"></a>
 
 <?php
 $postargs = array(
@@ -92,14 +92,14 @@ $postargs = array(
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																	<a href="<?php echo get_the_permalink(); ?>">
-																	<div class="col-md-3">
-																	<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
-																	<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
-																	</div>
-																	</a>
+																		<a href="<?php echo get_the_permalink(); ?>">
+																		<div class="col-md-3">
+																		<img src="<?php echo get_the_post_thumbnail_url() ?>" class="img-responsive">
+																		<p class="text-center peitu-title">- <?php echo get_the_title() ?> -</p>
+																		</div>
+																		</a>
 
-																	<?php
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -113,15 +113,15 @@ $postargs = array(
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																	<div class="col-md-5">
-																	<h2><?php echo get_the_title() ?></h2>
-																	<p class="desctext">
-																	<?php echo the_content() ?>
-																	</p>
-																	<a href="/?cat=208" ><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
-																	</div>
-																	<div class="col-md-7">
-																	<?php
+																		<div class="col-md-5">
+																		<h2><?php echo get_the_title() ?></h2>
+																		<p class="desctext">
+																		<?php echo the_content() ?>
+																		</p>
+																		<a href="/?cat=208" ><button class="newbutton" style="width:150px;margin-bottom:10px">进一步了解</button></a>
+																		</div>
+																		<div class="col-md-7">
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -137,16 +137,16 @@ while ($query1->have_posts()): $query1->the_post();
 ///head c"<?php echo get_the_permalink();
 
     ?>
-																	<div class="col-md-6">
-																	<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
-																	<h3><?php echo get_the_title() ?></h3>
-																	<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
-																	<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
-																	<p>YY直播：<?php echo get_post_meta(get_the_id(), 'YY直播', true) ?></p>
-																	<p>诵经内容：<?php echo get_post_meta(get_the_id(), '诵经内容', true) ?></p>
-																	<p><a href="<?php echo get_post_meta(get_the_id(), '报名', true) ?>"><button class="newbutton" style="width: 100%;" type="button">报名</button></a></p>
-																	</div>
-																	<?php
+																		<div class="col-md-6">
+																		<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
+																		<h3><?php echo get_the_title() ?></h3>
+																		<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
+																		<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
+																		<p>YY直播：<?php echo get_post_meta(get_the_id(), 'YY直播', true) ?></p>
+																		<p>诵经内容：<?php echo get_post_meta(get_the_id(), '诵经内容', true) ?></p>
+																		<p><a href="<?php echo get_post_meta(get_the_id(), '报名', true) ?>"><button class="newbutton" style="width: 100%;" type="button">报名</button></a></p>
+																		</div>
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -161,22 +161,22 @@ $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
 ///head c"<?php echo get_the_permalink();
     ?>
-																	<div class="col-md-6">
-																	<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>">
-																	<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
-																	</a>
-																	<h3><?php echo get_the_title() ?></h3>
-																	<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
-																	<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
-																	<p>诵经内容：<?php echo get_post_meta(get_the_id(), '诵经内容', true) ?></p>
-																	<p>&nbsp;</p>
-																	<p>
-																	<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton" style="width: 32%;" type="button">小瓜诵经</button></a>
-																	<a href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>&title=<?php echo get_post_meta(get_the_id(), '心得图库标题', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">心得</button></a>
-																	<a href="http://sutrachanting.longquanzs.org/index.php/nggallery/诵经活动-1/<?php echo get_the_title() ?>?page_id=2473&title=<?php echo get_post_meta(get_the_id(), '心得图库标题', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">照片</button></a>
-																	</p>
-																	</div>
-																	<?php
+																		<div class="col-md-6">
+																		<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>">
+																		<img style="width: 100%;height:191px;" src="<?php echo get_the_post_thumbnail_url(null, array(300, 183)) ?>">
+																		</a>
+																		<h3><?php echo get_the_title() ?></h3>
+																		<p>时间：<?php echo get_post_meta(get_the_id(), '时间', true) ?></p>
+																		<p>地点：<?php echo get_post_meta(get_the_id(), '地点', true) ?></p>
+																		<p>诵经内容：<?php echo get_post_meta(get_the_id(), '诵经内容', true) ?></p>
+																		<p>&nbsp;</p>
+																		<p>
+																		<a href="<?php echo get_post_meta(get_the_id(), '周报', true) ?>"><button class="newbutton" style="width: 32%;" type="button">小瓜诵经</button></a>
+																		<a href="<?php echo get_post_meta(get_the_id(), '心得', true) ?>&title=<?php echo get_post_meta(get_the_id(), '心得图库标题', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">心得</button></a>
+																		<a href="http://sutrachanting.longquanzs.org/index.php/nggallery/诵经活动-1/<?php echo get_the_title() ?>?page_id=2473&title=<?php echo get_post_meta(get_the_id(), '心得图库标题', true) ?>"><button class="newbutton"  style="width: 32%;" type="button">照片</button></a>
+																		</p>
+																		</div>
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -193,11 +193,11 @@ $postargs = array(
 $query1 = new WP_Query($postargs);
 while ($query1->have_posts()): $query1->the_post();
     ?>
-																	<h2><?php echo get_the_title() ?></h2>
-																	<p class="desctext">
-																	<?php echo the_content() ?>
-																	</p>
-																	<?php
+																		<h2><?php echo get_the_title() ?></h2>
+																		<p class="desctext">
+																		<?php echo the_content() ?>
+																		</p>
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
@@ -218,10 +218,10 @@ $n = 0;
 while ($query3->have_posts()): $query3->the_post();
 
     ?>
-																	<a href="<?php echo get_the_permalink(); ?>">
-																	<div class="booktitle"><?php echo get_the_title(); ?></div>
-																	<img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
-																	<?php
+																		<a href="<?php echo get_the_permalink(); ?>">
+																		<div class="booktitle"><?php echo get_the_title(); ?></div>
+																		<img src="/wp-content/themes/twentysixteen/img/book.jpg"></a>
+																		<?php
 endwhile;
 wp_reset_postdata();
 ?>
