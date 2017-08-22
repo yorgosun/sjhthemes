53842d3b4af52c07a0a262783df1acb696af95bd
Yorgo Sun <sunsx@sheng-inc.com>
2017-08-22 14:55:18 +0800

    尝试修改详情页图片置中的样式问题

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -8,6 +8,14 @@
  */
 
 ?>
+<style media="all">
+    img .aligncenter {
+        clear: both;
+        display: block;
+        margin: 0 auto 0 auto;
+        text-align: center;
+    }
+</style>
 <h1><?php the_title();?></h1>
 <p><?php echo get_post_meta(get_the_id(), 'author', true) ?>  时间：<?php the_time('Y-m-d')?></p>
 <div class="content-text">
